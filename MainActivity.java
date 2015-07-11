db0f7db1ca5fdc68262cd125cd1bb9ef5e301143
super-mario123 <nameicpico@gmail.com>
2015-07-11 09:09:49 +0800

    edit commit

--- MainActivity.java
+++ MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText userNameEdit;
     private EditText passwordEdit;
 
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
